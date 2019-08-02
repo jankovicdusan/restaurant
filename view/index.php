14c208bae4b75867c7f68c8e2e91071ab026e86d
jankovicdusan <jankovic.dushan@gmail.com>
2019-08-02 17:09:12 +0200

    small changes in navbar

--- view/index.php
+++ view/index.php
@@ -27,7 +27,7 @@
         <a class="nav-link" href="routes.php?page=reservation">Reserve Now<span class="sr-only">(current)</span></a>
 
       </li>
-      <li class="nav-item">
+      <li class="nav-item active">
       <a class="nav-link" href="routes.php?page=allreservations">All reservations<span class="sr-only">(current)</span></a>
       </li>
     </ul>
